f11c034ab9882e884317005075a43baaedfafa4b
danhab99 <dan.habot@gmail.com>
2018-01-09 10:56:33 +0200

    I implimented the map but it didn't work

--- ionic2-ts-sidemenu1/src/pages/page1/page1.ts
+++ ionic2-ts-sidemenu1/src/pages/page1/page1.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
     selector: 'page-page1',
@@ -8,10 +10,31 @@ import { NavController } from 'ionic-angular';
 })
 export class Page1 {
 
+    @ViewChild('map') mapElement: ElementRef;
+    map: any;
+    
     constructor(public navCtrl: NavController) {
+    }
+
+    ionViewDidLoad() {
+      this.loadMap();
     }
 
     onLink(url: string) {
         window.open(url);
+    }
+
+    loadMap() {
+
+        let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+        let mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
     }
 }
